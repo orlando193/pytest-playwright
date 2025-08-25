054b332f1f0fe0a3eb3c6b038fdb61b2fbb0d9cb
orlando193 <orlando.burdusel@gmail.com>
2025-08-25 17:16:51 +0300

    first commit

---
+++ tests/test_google.py
@@ -0,0 +1,15 @@
+import re
+from playwright.sync_api import expect
+
+def test_google_search(page):
+    page.goto("https://www.google.com/ncr")
+    try:
+        page.get_by_role("button",name="Accept all").click(timeout=5000)
+    except Exception as e:
+        print("no popup")   
+        
+    page.get_by_role("combobox", name="Search").fill("Playwright python")
+    
+    page.keyboard.press("Enter")
+    expect(page).to_have_title(re.compile("Playwright",re.IGNORECASE))     
+    
